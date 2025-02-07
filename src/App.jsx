3f87fe4655b5907eb6d0f2c04cc67c2273eb9a90
jubairzaman <jubair.zaman01@gmail.com>
2025-02-07 18:50:52 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Courses from './components/courses/Courses'
 import Support from './components/support/Support'
 import Careers from './components/careers/Careers'
 import Navbar1 from './components/navbar/Navbar1';
-import ContactUs from './components/contactUs/contactUs';
+import ContactUs from './components/contactUs/ContactUs';
 
 
 
